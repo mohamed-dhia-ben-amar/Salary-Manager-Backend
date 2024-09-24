4bae355f333739cbe8108e481f472eb6251e9690
mohamed-dhia-ben-amar <mohameddhia.benamar@esprit.tn>
2024-09-24 20:40:35 +0100

    Removed Unnecessary Methods From User Repository

--- src/main/java/com/dhaw/budgetmanagment/repositories/UserRepository.java
+++ src/main/java/com/dhaw/budgetmanagment/repositories/UserRepository.java
@@ -5,9 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByUsername(String username);
-
     Optional<User> findByEmail(String email);
-
-    Optional<User> findByEmailAndPassword(String username, String password);
 }
