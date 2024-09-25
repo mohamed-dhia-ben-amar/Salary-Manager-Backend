e530cef7940f6fc5064dfefaf2112bf123f59dd0
mohamed-dhia-ben-amar <mohameddhia.benamar@esprit.tn>
2024-09-25 04:15:02 +0100

    Added CORS Version 1.0

--- src/main/java/com/dhaw/budgetmanagment/config/WebConfig.java
+++ src/main/java/com/dhaw/budgetmanagment/config/WebConfig.java
@@ -16,8 +16,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
